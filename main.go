1eb7560267b5a0b24dfb95c01c66f9e7223c8c94
Alexander Scott <alexanderscott@outlook.com>
2023-01-03 14:01:53 +0000

    Run app via webserver

--- main.go
+++ main.go
@@ -2,13 +2,33 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"io"
+	"net/http"
+	"os"
 	"os/exec"
+	"time"
 )
 
 func main() {
 	fmt.Println(greet())
-	invokeBazel()
+
+	http.HandleFunc("/", getRoot)
+	http.HandleFunc("/bazel", getBazel)
+
+	server := &http.Server{
+		Addr:              ":3333",
+		ReadHeaderTimeout: time.Second,
+	}
+
+	err := server.ListenAndServe()
+	if errors.Is(err, http.ErrServerClosed) {
+		fmt.Println("Server closed")
+	} else if err != nil {
+		fmt.Println("Error starting server")
+		os.Exit(1)
+	}
 }
 
 func greet() string {
@@ -21,4 +41,21 @@ func invokeBazel() {
 		fmt.Println(err.Error())
 	}
 	fmt.Println(string(output))
+}
+
+func getRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("got request @ getRoot")
+	_, err := io.WriteString(w, "This is my website\n")
+	if err != nil {
+		fmt.Println("Failed to send string back along connection")
+	}
+}
+
+func getBazel(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("invoking bazel")
+	_, err := io.WriteString(w, "invoking bazel\n")
+	if err != nil {
+		fmt.Println("Failed to send string back along connection")
+	}
+	invokeBazel()
 }
