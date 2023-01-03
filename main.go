b180563c8a454cbdb349a9796b3e3823f1f95c1c
Alexander Scott <alexanderscott@outlook.com>
2023-01-03 13:08:36 +0000

    Create a function that invokes bazel

--- main.go
+++ main.go
@@ -1,12 +1,24 @@
 // Application which greets you.
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os/exec"
+)
 
 func main() {
 	fmt.Println(greet())
+	invokeBazel()
 }
 
 func greet() string {
 	return "Hi!"
+}
+
+func invokeBazel() {
+	output, err := exec.Command("bazel", "version").Output()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	fmt.Println(string(output))
 }
